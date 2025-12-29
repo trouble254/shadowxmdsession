5a5af45a3d991ce0b905e247cc31fe97c4b56495
trouble254 <herokuotundo@gmail.com>
2025-12-29 13:02:09 +0300

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -68,12 +68,12 @@ ______________________________________
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1
-║❒ *Owner:* _https://wa.me/message/THZ3I25BYZM2E1_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD_
-║❒ *WaGroup:* _https://chat.whatsapp.com/FF6YuOZTAVB6Lu65cnY5BN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j_
-║❒ *Plugins:* _https://github.com/Itxxwasi 
+║❒ *Ytube:* _youtube.com/@trouble254
+║❒ *Owner:* _https://wa.me/message/254742215053_
+║❒ *Repo:* _https://github.com/trouble254/shadow-XMD_
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029VbBM1Kw8kyyFKORNQv1q_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VbBM1Kw8kyyFKORNQv1q_
+║❒ *Plugins:* _https://github.com/trouble254/shadow-XMD 
 ╚════════════════════════╝
 _____________________________________
 	
