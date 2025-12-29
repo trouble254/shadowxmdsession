703b5c179c1eeba8be02d96024835e7153759e75
trouble254 <herokuotundo@gmail.com>
2025-12-29 15:16:27 +0300

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,152 @@
+
+const { giftedid } = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router();
+const pino = require("pino");
+const { Storage, File } = require("megajs");
+
+const {
+    default: Gifted_Tech,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("gifted-baileys");
+
+function randomMegaId(length = 6, numberLength = 4) {
+                      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                      let result = '';
+                      for (let i = 0; i < length; i++) {
+                      result += characters.charAt(Math.floor(Math.random() * characters.length));
+                        }
+                       const number = Math.floor(Math.random() * Math.pow(10, numberLength));
+                        return `${result}${number}`;
+                        }
+
+async function uploadCredsToMega(credsPath) {
+    try {
+        const storage = await new Storage({
+  email: 'www.qadeem950@gmail.com', // // Your Mega A/c Email Here
+  password: '@anayatkhan1' // Your Mega A/c Password Here
+}).ready
+        console.log('Mega storage initialized.');
+        if (!fs.existsSync(credsPath)) {
+            throw new Error(`File not found: ${credsPath}`);
+        }
+       const fileSize = fs.statSync(credsPath).size;
+        const uploadResult = await storage.upload({
+            name: `${randomMegaId()}.json`,
+            size: fileSize
+        }, fs.createReadStream(credsPath)).complete;
+        console.log('Session successfully uploaded to Mega.');
+        const fileNode = storage.files[uploadResult.nodeId];
+        const megaUrl = await fileNode.link();
+        console.log(`Session Url: ${megaUrl}`);
+        return megaUrl;
+    } catch (error) {
+        console.error('Error uploading to Mega:', error);
+        throw error;
+    }
+}
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
+router.get('/', async (req, res) => {
+    const id = giftedid();
+    let num = req.query.number;
+    async function GIFTED_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let Gifted = Gifted_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: Browsers.macOS("Safari")
+            });
+            if (!Gifted.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Gifted.requestPairingCode(num);
+                console.log(`Your Code: ${code}`);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            Gifted.ev.on('creds.update', saveCreds);
+            Gifted.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection == "open") {
+                    await delay(5000);
+                    const filePath = __dirname + `/temp/${id}/creds.json`;
+                    if (!fs.existsSync(filePath)) {
+                        console.error("File not found:", filePath);
+                        return;
+                    }
+
+          const megaUrl = await uploadCredsToMega(filePath);
+          const sid = megaUrl.includes("https://mega.nz/file/")
+            ? 'ARSL~' + megaUrl.split("https://mega.nz/file/")[1]
+            : 'Error: Invalid URL';
+          
+          console.log(`Session ID: ${sid}`);
+
+                    const session = await Gifted.sendMessage(Gifted.user.id, { text: sid }, { disappearingMessagesInChat: true, ephemeralExpiration: 600, });
+
+                    const GIFTED_TEXT = `
+*✅sᴇssɪᴏɴ ɪᴅ ɢᴇɴᴇʀᴀᴛᴇᴅ✅*
+______________________________
+*🎉 SESSION GENERATED SUCCESSFULLY! ✅*
+
+*💪 Empowering Your Experience with Silva MD Bot*
+
+*🌟 Show your support by giving our repo a star! 🌟*
+🔗 https://github.com/SilvaTechB/silva-md-bot
+
+*💭 Need help? Join our support groups:*
+📢 💬
+https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v
+
+*📚 Learn & Explore More with Tutorials:*
+🪄 YouTube Channel https://www.youtube.com/@silvaedits254
+
+*🥀 Powered by Silva MD Bot & Silva Tech Inc 🥀*
+*Together, we build the future of automation! 🚀*
+______________________________
+
+Use your Session ID Above to Deploy your Bot.
+Check on YouTube Channel for Deployment Procedure(Ensure you have Github Account and Billed Heroku Account First.)
+Don't Forget To Give Star⭐ To My Repo`;
+                    await Gifted.sendMessage(Gifted.user.id, { text: GIFTED_TEXT }, { quoted: session },  { disappearingMessagesInChat: true, ephemeralExpiration: 600, });
+
+                    await delay(100);
+                    await Gifted.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    GIFTED_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.error("Service Has Been Restarted:", err);
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "Service is Currently Unavailable" });
+            }
+        }
+    }
+
+    return await GIFTED_PAIR_CODE();
+});
+
+module.exports = router;
